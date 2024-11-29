04eed6931b6ddd57ff78c264fcd99fe9ad784357
Дарья Заремская <dashaz1801@gmail.com>
2024-11-29 10:06:58 +0700

    Добавление структур

--- laba2/laba2.cpp
+++ laba2/laba2.cpp
@@ -1,20 +1,66 @@
-﻿// laba2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <conio.h>
+#include <locale.h>
 
-#include <iostream>
+#define SIZE 9
+#define MINES 18
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+//структура ячейки
+struct Cell {
+    int x, y;
+    int isMine;
+    int isOpened;
+    int isFlagged;
+    int mineCount;
+};
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+//структура поля
+struct Field {
+    Cell cells[SIZE][SIZE];
+};
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+//структура игрока
+struct Player {
+    int score;
+    int time;
+};
+
+//структура игры
+struct Game {
+    Field field;
+    Player player;
+    int isRunning;
+    int gameStarted; // Флаг начала игры
+};
+
+//структура меню
+struct Menu {
+    char options[4][20];
+    int selectedOption;
+};
+
+//структура настроеек
+struct Settings {
+    int size;
+    int mines;
+    int difficulty; // Уровень сложности (легкий, средний, сложный)
+};
+
+//структура игровой статистики
+struct Statistics {
+    int wins;
+    int losses;
+    long totalTime;
+};
+
+//структура игрового процесса
+struct GameProgress {
+    int x, y;
+    int isOpened;
+    int isFlagged;
+};
